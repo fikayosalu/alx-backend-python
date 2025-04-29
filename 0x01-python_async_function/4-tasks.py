e70faf78331cdf37e37dc29b790d2c36ffb49230
fikayosalu <salufikayo@gmail.com>
2025-04-29 20:22:27 +0100

    Task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,13 +3,13 @@
 import asyncio
 from typing import List
 from types import CoroutineType
-
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Returns a list of floats in the order of how the
-    coroutine was completed"""
+    coroutine was completed
+    """
     arr: List[asyncio.Future] = [task_wait_random(max_delay) for _ in range(n)]
     results: List[float] = []
     for item in asyncio.as_completed(arr):
