2af6529a289cc90d15570ba3c87ec5a033633f2c
fikayosalu <salufikayo@gmail.com>
2025-04-29 17:33:28 +0100

    Task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """ 1-concurrent_coroutines.py module contains async function"""
 import asyncio
-wait_random: object = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list:
     """Returns a list of awaitables in order of completion"""
     arr: list[float] = [wait_random(max_delay) for _ in range(n)]
-    results: list = []
+    results: list[float] = []
     for item in asyncio.as_completed(arr):
         res: float = await item
         results.append(res)
