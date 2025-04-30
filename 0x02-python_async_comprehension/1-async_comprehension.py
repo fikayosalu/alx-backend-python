00bbaa0dec650031efae399b3bd77e59692aecb5
fikayosalu <salufikayo@gmail.com>
2025-04-30 12:01:53 +0100

    Task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Module that demonstrates asynchronous comprehension in Python."""
 import asyncio
+from typing import List
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """ Returns:list[float]: A list containing 10 float
     numbers generated asynchronously"""
-    result: list[float] = [x async for x in async_generator()]
+    result: List[float] = [x async for x in async_generator()]
     return result
