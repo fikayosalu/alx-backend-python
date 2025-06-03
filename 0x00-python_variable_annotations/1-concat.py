72f7421149a4b3959f55046ed3b1acdb72f69b63
fikayosalu <salufikayo@gmail.com>
2025-06-03 13:21:36 +0100

    Annotation in task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""1-concat module contains a function that is statically typed"""
+""" 1-concat """
 
 
 def concat(str1: str, str2: str) -> str:
-    """ The concat function takes two strings and \
+    """ The concat function takes two strings and
     returns a concatenated string """
-    return f"{str1} {str2}"
+    return str1 + str2
